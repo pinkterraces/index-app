a43f681e04d34094aa2679762a2df845b743f710
pinkterraces <108399126+pinkterraces@users.noreply.github.com>
2022-08-11 11:56:37 +0200

    Fixed typo in code

--- js/script.js
+++ js/script.js
@@ -2,25 +2,25 @@ let pokemonList = [
     let pokemon1 = {
         name: 'Raichu',
         height: 0.8,
-        type: ['Ground', 'Flying', 'Steel', 'Electric'],
+        types: ['Ground', 'Flying', 'Steel', 'Electric'],
         abilities: ['Static', 'Lightningrod']
     }
     let pokemon2 = {
         name: 'Krookodile',
         height: 1.5,
-        type: ['Water', 'Grass', 'Ice', 'Fighting', 'Bug', 'Fairy'],
+        types: ['Water', 'Grass', 'Ice', 'Fighting', 'Bug', 'Fairy'],
         abilities: ['Intimidate', 'Anger-point', 'Moxie']
     }
     let pokemon3 = {
         name: 'Jynx',
         height: 1.4,
-        type: ['Rock', 'Steel', 'Fire', 'Bug', 'Ghost', 'Dragon'],
+        types: ['Rock', 'Steel', 'Fire', 'Bug', 'Ghost', 'Dragon'],
         abilities:['Oblivious', 'Dry-skin', 'Forewarn']
     }
     let pokemon4 = {
         name: 'Regice',
         height: 1.8,
-        type: ['Fighting', 'Rock', 'Steel', 'Fire', 'Ice'],
+        types: ['Fighting', 'Rock', 'Steel', 'Fire', 'Ice'],
         abilities:['Clear-body', 'Ice-body']
     }
 ]
