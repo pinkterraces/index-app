5460210d34c6a4f42cc57b3c01cd2cf74612bcf0
pinkterraces <108399126+pinkterraces@users.noreply.github.com>
2022-08-17 19:22:50 +0200

    Update js to output pokemon names and height

--- js/script.js
+++ js/script.js
@@ -1,27 +1,38 @@
 let pokemonList = [
-    let pokemon1 = {
+    pokemon1 = {
         name: 'Raichu',
         height: 0.8,
         types: ['Ground', 'Flying', 'Steel', 'Electric'],
         abilities: ['Static', 'Lightningrod']
-    }
-    let pokemon2 = {
+    },
+    pokemon2 = {
         name: 'Krookodile',
         height: 1.5,
         types: ['Water', 'Grass', 'Ice', 'Fighting', 'Bug', 'Fairy'],
         abilities: ['Intimidate', 'Anger-point', 'Moxie']
-    }
-    let pokemon3 = {
+    },
+    pokemon3 = {
         name: 'Jynx',
         height: 1.4,
         types: ['Rock', 'Steel', 'Fire', 'Bug', 'Ghost', 'Dragon'],
         abilities:['Oblivious', 'Dry-skin', 'Forewarn']
-    }
-    let pokemon4 = {
+    },
+    pokemon4 = {
         name: 'Regice',
         height: 1.8,
         types: ['Fighting', 'Rock', 'Steel', 'Fire', 'Ice'],
         abilities:['Clear-body', 'Ice-body']
     }
 ]
+
+//Formats the output
+let pokeName__open = "<p class='pokeList'>"
+let pokeName__close = "</p>"
+let pokeHeight__open = " <span class='pokelist__height'>Height: ";
+let pokeHeight__close = "</span>"
+
+for (let i = 1; i < pokemonList.length; i++) {
+    document.write(pokeName__open + pokemonList[i].name + pokeHeight__open + pokemonList[i].height + pokeHeight__close + pokeName__close);
+    console.log(pokemonList[i].name);
+}
 
