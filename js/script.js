1911db3d6aa511e57bf3c07efd2e806e71b19f46
pinkterraces <108399126+pinkterraces@users.noreply.github.com>
2022-09-09 15:13:35 +0200

    Added getAll and add functions

--- js/script.js
+++ js/script.js
@@ -1,28 +1,58 @@
-let pokemonList = [
-    pokemon1 = {
-        name: 'Raichu',
-        height: 0.8,
-        types: ['Ground', 'Flying', 'Steel', 'Electric'],
-        abilities: ['Static', 'Lightningrod']
-    },
-    pokemon2 = {
-        name: 'Krookodile',
-        height: 1.5,
-        types: ['Water', 'Grass', 'Ice', 'Fighting', 'Bug', 'Fairy'],
-        abilities: ['Intimidate', 'Anger-point', 'Moxie']
-    },
-    pokemon3 = {
-        name: 'Jynx',
-        height: 1.4,
-        types: ['Rock', 'Steel', 'Fire', 'Bug', 'Ghost', 'Dragon'],
-        abilities:['Oblivious', 'Dry-skin', 'Forewarn']
-    },
-    pokemon4 = {
-        name: 'Regice',
-        height: 1.8,
-        types: ['Fighting', 'Rock', 'Steel', 'Fire', 'Ice'],
-        abilities:['Clear-body', 'Ice-body']
-    } ];
+let pokemonRepository = (function()  {
+
+    let pokemonList = [
+        pokemon1 = {
+            name: 'Raichu',
+            height: 0.8,
+            types: ['Ground', 'Flying', 'Steel', 'Electric'],
+            abilities: ['Static', 'Lightningrod']
+        },
+        pokemon2 = {
+            name: 'Krookodile',
+            height: 1.5,
+            types: ['Water', 'Grass', 'Ice', 'Fighting', 'Bug', 'Fairy'],
+            abilities: ['Intimidate', 'Anger-point', 'Moxie']
+        },
+        pokemon3 = {
+            name: 'Jynx',
+            height: 1.4,
+            types: ['Rock', 'Steel', 'Fire', 'Bug', 'Ghost', 'Dragon'],
+            abilities:['Oblivious', 'Dry-skin', 'Forewarn']
+        },
+        pokemon4 = {
+            name: 'Regice',
+            height: 1.8,
+            types: ['Fighting', 'Rock', 'Steel', 'Fire', 'Ice'],
+            abilities:['Clear-body', 'Ice-body']
+        } ];
+
+        function add(pokemonItem) {
+            pokemonList.push(pokemonItem);
+        }
+
+        function getAll() {
+            return pokemonList;
+        }
+
+        return {
+            add: add,
+            getAll: getAll            
+        };
+
+})();
+
+let pokemon5 = {
+    name: 'James',
+    height: 1.4,
+    types: ['Rock', 'Steel', 'Fire', 'Bug', 'Ghost', 'Dragon'],
+    abilities:['Oblivious', 'Dry-skin', 'Forewarn']
+};
+
+//Calls Pokemon array
+console.log(pokemonRepository.getAll());
+//Adds Pokemon
+console.log(pokemonRepository.add(pokemon5));
+
 
 //Formats bid pokemon text
 let bigPokemon = " <b><span class='emoji'>&#9889;</span> Wow, that's big!</b> <span class='emoji'>&#9889;</span>";
