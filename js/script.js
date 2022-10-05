74cd93e2af9058d0cbf1c9a8235d93dfb744767f
pinkterraces <108399126+pinkterraces@users.noreply.github.com>
2022-10-05 19:58:49 +0200

    Added Ajax to call repository WIP

--- js/script.js
+++ js/script.js
@@ -33,6 +33,8 @@ let pokemonRepository = (function()  {
             }
         };
 
+        let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
         function getAll() {
             return pokemonList;
         };
@@ -54,16 +56,54 @@ let pokemonRepository = (function()  {
         };
 
         function showDetails(pokemon) {
-            console.log(pokemon.name);
+            loadDetails(pokemon).then(function() {
+                console.log(pokemon)
+            });
+        };
+
+        function loadList() {
+            return fetch(apiUrl).then(function(response) {
+                return response.json();
+            }).then(function(json) {
+                json.results.forEach(function(item) {
+                   let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                   };
+                   add(pokemon);
+                });
+            }).catch(function(e) {
+                console.error(e);
+            })
+        };
+
+        function loadDetails(item) {
+            let url = item.detailsUrl;
+            return fetch(url).then(function(response) {
+                return response.json();
+            }).then(function(details) {
+                item.imageUrl = details.sprites.front_default;
+                item.height = details.height;
+                item.types = details.types;
+            }).catch(function(e) {
+                console.error(e);
+            });
         };
 
         return {
             add: add,
             getAll: getAll,
-            addListItem: addListItem
+            addListItem: addListItem,
+            loadList: loadList
         };      
 
 })();
+
+pokemonRepository.loadList().then(function() {
+    pokemonRepository.getAll().forEach(function(pokemon){
+        pokemonRepository.addListItem(pokemon);
+    });
+});
 
 //New Pokemon Data
 let pokemon5 = {
@@ -91,6 +131,6 @@ pokemonRepository.add(pokemon6);
 let bigPokemon = " <b><span class='emoji'>&#9889;</span> Wow, that's big!</b> <span class='emoji'>&#9889;</span>";
 
 //Assesses height of Pokemon and writes results to document
-pokemonRepository.getAll().forEach(function(pokemon) {
+/* pokemonRepository.getAll().forEach(function(pokemon) {
     pokemonRepository.addListItem(pokemon);
-});
+}); */
